5f23fdf7782040a521af532de071359464442747
Mike <mullishmulla@gmail.com>
2021-08-25 03:04:13 +0000

    react-native-profile-page

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet, TextInput} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+
+
+function App() {
+  return <View style={styles.container}>
+    <Image source={require('./assets/mina.jpg')} style={styles.image} />
+
+    <View style={styles.detailContainer}>
+      <Text style={styles.firstLabel}>School</Text>
+      <TextInput style={styles.labelText} />
     </View>
-  );
+
+    <View style={styles.detailContainer}>
+      <Text style={styles.firstLabel}>Email Address</Text>
+      <TextInput style={styles.labelText} />
+    </View>
+
+    <View style={styles.detailContainer}>
+      <Text style={styles.textColors}>Name</Text>
+      <TextInput style={styles.labelText} />
+    </View>
+
+    <View style={styles.detailContainer}>
+      <Text style={styles.textColors}>Nick Name</Text>
+      <TextInput style={styles.labelText} />
+    </View>
+
+    <View style={[styles.detailContainer, styles.lastBox]}>
+      <Text style={styles.textColors}>Emergency Contact</Text>
+      <TextInput style={styles.labelText} />
+    </View>
+
+    <View style={styles.buttonBox}>
+      <Text style={styles.buttonText}>UPDATE PROFILE</Text>
+    </View>
+
+  </View>
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //backgroundColor: "grey",
   },
-});
+  image: {
+    marginTop: 40,
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 30,
+    alignSelf: "center"
+  },
+  detailContainer: {
+    marginHorizontal: 50
+  },
+
+  labelText: {
+    borderColor: "lightgrey",
+    borderBottomWidth: 1,
+    fontSize: 20,
+    marginVertical: 5
+  },
+  firstLabel: {
+    color: "grey"
+  },
+  textColors: {
+    color: "#1d87fb"
+  },
+  lastBox: {
+    marginBottom: 30
+  },
+  buttonBox: {
+    width: 260,
+    height: 60,
+    borderRadius:80,
+    padding: 10,
+    alignSelf:"center",
+    backgroundColor: "#1d87fb",
+    
+  },
+  buttonText: {
+    color: "#ffff",
+    textAlign: "center",
+    padding: 10,
+    fontSize: 20
+  }
+
+})
+
+export default App;
